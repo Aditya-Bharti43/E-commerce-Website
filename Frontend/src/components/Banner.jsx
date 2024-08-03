fdbb978952baf3bbe4659f5d371194cef1f1cc4a
Aditya-Bharti43 <159432444+Aditya-Bharti43@users.noreply.github.com>
2024-08-03 10:45:28 +0530

    Banner.jsx

---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import'./Banner.css'
+
+const Banner = () => {
+  return (
+    <div>
+      <img className='banner' src="https://assets.ajio.com/cms/AJIO/WEB/D-1.0-UHP-26062024-UrgencyStrip-Z1-Last3DaysOfSale.gif" alt="" />
+    </div>
+  )
+}
+
+export default Banner
